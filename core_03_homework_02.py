f51915b626db02906df651dcbf1f05ad24da2c1e
Volodymyr Vakula <vakula.volodymyr@gmail.com>
2025-02-02 20:15:17 +0200

    First version of problem 2

---
+++ core_03_homework_02.py
@@ -0,0 +1,21 @@
+import random
+
+# Function to get given number of random integer values within given range
+def get_numbers_ticket(min_value: int, max_value: int, quantity: int) -> list:
+    """
+    Gets a given number of random integer values within a given range
+
+    Input:
+    :param min_value: integer
+    :param max_value: integer
+    :param quantity: integer
+    
+    Output:
+    :return: list
+    """
+    if min_value < 1 or max_value > 1000 or not min_value <= quantity <= max_value:
+        return []
+    set_of_numbers = set()
+    while len(set_of_numbers) < quantity:
+        set_of_numbers.add(random.randrange(min_value, max_value + 1))
+    return sorted(list(set_of_numbers))
